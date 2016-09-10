c06e996e02d9fce86686b2629df97d1e1c0b586c
Sahil Agarwal <sahil.agarwal@xoom.com>
2016-09-10 01:44:22 -0700

    Added light / dark mode and a round-up feature

--- goodtips/SettingsViewController.swift
+++ goodtips/SettingsViewController.swift
@@ -10,37 +10,72 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var tipText: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
-    @IBOutlet weak var tipPicker: UIPickerView!
+    @IBOutlet weak var modeText: UILabel!
+    @IBOutlet weak var modeSwitch: UISwitch!
+    @IBOutlet weak var customTipText: UILabel!
     @IBOutlet weak var customTip: UITextField!
+    @IBOutlet weak var percentSignText: UILabel!
+    
+    let defaults = NSUserDefaults.standardUserDefaults()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let defaults = NSUserDefaults.standardUserDefaults()
         let tipSelectedSegmentIndex = defaults.integerForKey("tipSelectedSegmentIndex")
         tipControl.selectedSegmentIndex = tipSelectedSegmentIndex
         
+        // set colors and mode switch
+        let mode = defaults.boolForKey("mode")
+        modeSwitch.on = mode
+        setColors()
+        
+        // set tip and custom tip
         customTip.text = String(defaults.integerForKey("customTip"))
         let customTipLabel = customTip.text! + "%"
         tipControl.setTitle(customTipLabel, forSegmentAtIndex: 3)
     }
     
+    func setColors() {
+        let mode = defaults.boolForKey("mode")
+        if(mode) {
+            self.view.backgroundColor = UIColor.whiteColor()
+            tipText.textColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            tipControl.tintColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            modeText.textColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            customTipText.textColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            percentSignText.textColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+        } else {
+            self.view.backgroundColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            tipText.textColor = UIColor.whiteColor()
+            tipControl.tintColor = UIColor.whiteColor()
+            modeText.textColor = UIColor.whiteColor()
+            customTipText.textColor = UIColor.whiteColor()
+            percentSignText.textColor = UIColor.whiteColor()
+        }
+    }
+    
     @IBAction func saveIndex(sender: UISegmentedControl) {
-        let defaults = NSUserDefaults.standardUserDefaults()
         defaults.setInteger(tipControl.selectedSegmentIndex, forKey: "tipSelectedSegmentIndex")
         defaults.synchronize()
     }
     
     @IBAction func saveCustomTipAndUpdateLabel(sender: AnyObject) {
-        let defaults = NSUserDefaults.standardUserDefaults()
         let customTipPercent = customTip.text! == "" ? "0" : customTip.text!
         defaults.setInteger(Int(customTipPercent)!, forKey: "customTip")
         defaults.synchronize()
         
         let customTipLabel = customTipPercent + "%"
         tipControl.setTitle(customTipLabel, forSegmentAtIndex: 3)
+    }
+    
+    @IBAction func changeMode(sender: AnyObject) {
+        let mode = modeSwitch.on
+        defaults.setBool(mode, forKey: "mode")
+        defaults.synchronize()
+        setColors()
     }
     
     @IBAction func clearField(sender: AnyObject) {
--- goodtips/TipViewController.swift
+++ goodtips/TipViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billText: UILabel!
     @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipText: UILabel!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalText: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
-    @IBOutlet weak var venmoButton: UIButton!
+    @IBOutlet weak var RoundUpButton: UIButton!
+    @IBOutlet weak var openText: UILabel!
+    
     let currencyFormatter = NSNumberFormatter()
+    let defaults = NSUserDefaults.standardUserDefaults()
+    var total = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +31,10 @@ class ViewController: UIViewController {
     }
     
     override func viewWillAppear(animated: Bool) {
+        // set colors
+        setColors()
         
         // load data
-        let defaults = NSUserDefaults.standardUserDefaults()
         let tipSelectedSegmentIndex = defaults.integerForKey("tipSelectedSegmentIndex")
         tipControl.selectedSegmentIndex = tipSelectedSegmentIndex
         billField.text! = defaults.objectForKey("billField") == nil ? "0" : defaults.objectForKey("billField") as! String
@@ -33,6 +42,9 @@ class ViewController: UIViewController {
         // set custom tip label
         let customTipLabel = String(defaults.integerForKey("customTip")) + "%"
         tipControl.setTitle(customTipLabel, forSegmentAtIndex: 3)
+        
+        // set tipPercentageLabel
+        setTipPercentageLabel()
         
         // calculate tip
         self.calculateTip("")
@@ -50,6 +62,41 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     
+    func setColors() {
+        let mode = defaults.boolForKey("mode")
+        if(mode) {
+            self.view.backgroundColor = UIColor.whiteColor()
+            billText.textColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            tipText.textColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            tipPercentageLabel.textColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            tipLabel.textColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            totalText.textColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            totalLabel.textColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            tipControl.tintColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            RoundUpButton.backgroundColor = UIColor.whiteColor()
+            RoundUpButton.titleLabel?.textColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            openText.textColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+        } else {
+            self.view.backgroundColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            billText.textColor = UIColor.whiteColor()
+            tipText.textColor = UIColor.whiteColor()
+            tipPercentageLabel.textColor = UIColor.whiteColor()
+            tipLabel.textColor = UIColor.whiteColor()
+            totalText.textColor = UIColor.whiteColor()
+            totalLabel.textColor = UIColor.whiteColor()
+            tipControl.tintColor = UIColor.whiteColor()
+            RoundUpButton.backgroundColor = UIColor(red: 29/255, green: 162/255, blue: 19/255, alpha: 1.0)
+            RoundUpButton.titleLabel?.textColor = UIColor.whiteColor()
+            openText.textColor = UIColor.whiteColor()
+        }
+    }
+    
+    func setTipPercentageLabel() {
+        let tip = total - Double(billField.text!)!
+        let tipPercent = total == 0 ? " " : String(Int(round(tip / (total-tip) * 100)))
+        tipPercentageLabel.text = "(" + tipPercent + "%)"
+    }
+    
     func changeNumberToCurrency(number : Double) -> String {
         currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
         currencyFormatter.currencyCode = NSLocale.currentLocale().displayNameForKey(NSLocaleCurrencySymbol, value: NSLocaleCurrencyCode)
@@ -58,7 +105,6 @@ class ViewController: UIViewController {
     }
     
     func getCustomTipPercentage() -> Double {
-        let defaults = NSUserDefaults.standardUserDefaults()
         return Double(defaults.integerForKey("customTip")) / 100
     }
     
@@ -68,20 +114,33 @@ class ViewController: UIViewController {
         let userInput = Double(billField.text!) == nil ? 0 : Double(billField.text!)
         
         let tip = userInput! * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = userInput! + tip
+        total = userInput! + tip
         
         tipLabel.text = changeNumberToCurrency(tip)
         totalLabel.text = changeNumberToCurrency(total)
+        
+        // set tipPercentageLabel
+        setTipPercentageLabel()
+    }
+    
+    @IBAction func RoundUpTotal(sender: AnyObject) {
+        let ceilTotal = ceil(total)
+        let ceilTip = ceilTotal - Double(billField.text!)!
+        
+        tipLabel.text = changeNumberToCurrency(ceilTip)
+        totalLabel.text = changeNumberToCurrency(ceilTotal)
+        total = ceilTotal
+        
+        // set tipPercentageLabel
+        setTipPercentageLabel()
     }
 
     @IBAction func saveTipSelectorIndex(sender: AnyObject) {
-        let defaults = NSUserDefaults.standardUserDefaults()
         defaults.setInteger(tipControl.selectedSegmentIndex, forKey: "tipSelectedSegmentIndex")
         defaults.synchronize()
     }
     
     @IBAction func saveBillAmount(sender: AnyObject) {
-        let defaults = NSUserDefaults.standardUserDefaults()
         defaults.setObject(billField.text!, forKey: "billField")
         defaults.synchronize()
     }
